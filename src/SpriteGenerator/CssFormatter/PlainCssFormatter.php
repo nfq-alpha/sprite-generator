2c1cd36f41c698e35ee1be24b9daffee76e022ec
Kazimieras Butkus <kazimieras@nfq.lt>
2014-09-12 18:59:23 +0000

    css generation refactored to use twig templates

--- src/SpriteGenerator/CssFormatter/PlainCssFormatter.php
+++ src/SpriteGenerator/CssFormatter/PlainCssFormatter.php
@@ -2,45 +2,10 @@
 
 namespace SpriteGenerator\CssFormatter;
 
-class PlainCssFormatter implements CssFormatterInterface
+class PlainCssFormatter extends BaseFormatter implements CssFormatterInterface
 {
     /**
-     * @param array $sourceImages
-     * @param string $spriteClass
-     * @param string $spriteImageName
-     * @return string
-     *
-     * @TODO: move formatting to twig template
+     * @var string
      */
-    public function format(array $sourceImages, $spriteClass, $spriteImageName)
-    {
-        $css = '';
-        $this->addCssLine($css, ".{$spriteClass} {");
-        $this->addCssLine($css, "\tbackground: url({$spriteImageName}) no-repeat;");
-        $this->addCssLine($css, "}");
-        $this->addCssLine($css, "");
-
-        foreach ($sourceImages as $key => $image) {
-            $this->addCssLine($css, ".{$key} {");
-
-            $this->addCssLine($css, "\twidth: {$image['width']}px;");
-            $this->addCssLine($css, "\theight: {$image['height']}px;");
-
-            $this->addCssLine($css, "\tbackground-position: -{$image['pos_x']}px -{$image['pos_y']}px;");
-            $this->addCssLine($css, "}");
-
-            $this->addCssLine($css, "");
-        }
-
-        return $css;
-    }
-
-    /**
-     * @param $wholeCss string
-     * @param $addCss string
-     */
-    public function addCssLine(&$wholeCss, $addCss)
-    {
-        $wholeCss = $wholeCss . "$addCss\r\n";
-    }
+    protected $template = 'plainCss.html.twig';
 }
--- src/SpriteGenerator/CssFormatter/SassFormatter.php
+++ src/SpriteGenerator/CssFormatter/SassFormatter.php
@@ -2,45 +2,10 @@
 
 namespace SpriteGenerator\CssFormatter;
 
-class SassFormatter implements CssFormatterInterface
+class SassFormatter extends BaseFormatter implements CssFormatterInterface
 {
     /**
-     * @param array $sourceImages
-     * @param string $spriteClass
-     * @param string $spriteImageName
-     * @return string
-     *
-     * @TODO: move formatting to twig template
+     * @var string
      */
-    public function format(array $sourceImages, $spriteClass, $spriteImageName)
-    {
-        $css = '';
-        $this->addCssLine($css, ".{$spriteClass} {");
-        $this->addCssLine($css, "\tbackground: url({$spriteImageName}) no-repeat;");
-        $this->addCssLine($css, "");
-
-        foreach ($sourceImages as $key => $image) {
-            $this->addCssLine($css, "\t.{$key} {");
-
-            $this->addCssLine($css, "\t\twidth: {$image['width']}px;");
-            $this->addCssLine($css, "\t\theight: {$image['height']}px;");
-
-            $this->addCssLine($css, "\t\tbackground-position: -{$image['pos_x']}px -{$image['pos_y']}px;");
-            $this->addCssLine($css, "\t}");
-        }
-
-        $this->addCssLine($css, "}");
-        $this->addCssLine($css, "");
-
-        return $css;
-    }
-
-    /**
-     * @param $wholeCss string
-     * @param $addCss string
-     */
-    public function addCssLine(&$wholeCss, $addCss)
-    {
-        $wholeCss = $wholeCss . "$addCss\r\n";
-    }
+    protected $template = 'sass.html.twig';
 }
